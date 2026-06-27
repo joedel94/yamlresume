48d4e91c34672d9ac357c63d99161ba59efde008
joedel94 <joedel94@gmail.com>
2026-06-26 21:42:34 -0400

    Updated for streamlines logic and included error handling

--- miscs/private/Get-AbsoluteOutputPath.ps1
+++ miscs/private/Get-AbsoluteOutputPath.ps1
@@ -2,6 +2,12 @@
 .SYNOPSIS
     Private helper that resolves and normalizes output paths
 
+.DESCRIPTION
+    This function resolves a relative path to an absolute path and normalizes 
+    it to use forward slashes for reference by the Docker container. 
+    Additionally the path will be checked to confirm it exists and if it does 
+    not it will be created.
+
 .PARAMETER OutputPath
     The relative path to the YAML file to process. Defaults to "my-resume.yml".
 
@@ -14,14 +20,22 @@ function Get-AbsoluteOutputPath {
     [CmdletBinding()]
     param(
         [Parameter(ValueFromPipeline, Mandatory = $true)]
-        [string]$OutputPath
+        [string]$Path
     )
 
-    $AbsoluteOutputPath = (Resolve-Path -Path $OutputPath -ErrorAction SilentlyContinue).Path
-
-    if (-not $AbsoluteOutputPath) {
-        $AbsoluteOutputPath = (New-Item -Path $OutputPath -ItemType Directory -Force).FullName
+    if (Test-Path -Path $Path) {
+        try {
+            Write-Verbose "Path '$Path' not found. Creating directory."
+            $AbsoluteOutputPath = (New-Item -Path $Path -ItemType Directory).FullName
+        }
+        catch {
+            throw "Issue encountered when attempting to make new directory $Path, full error: $($_.ErrorDetails.Message)"
+        }
+    }
+    else {
+        $AbsoluteOutputPath = (Resolve-Path -LiteralPath $Path).Path
     }
 
+    #Normalize the path to use forward slashes
     return $AbsoluteOutputPath -replace '\\', '/'
 }
